adc0d5804f57762ccf90d577c3880e691ffdfdc0
Piero Angelo <piero.morales@utec.edu.pe>
2019-10-02 01:27:35 -0500

    Final version UVA

--- main.cpp
+++ main.cpp
@@ -5,74 +5,78 @@ using namespace std;
 class Hoja {
 private:
     int filas = 0, columnas = 0;
-    string **matrizPrimitiva = nullptr;
-    int **matrizFinal = nullptr;
+    vector < vector <string> > matriz;
 
 public:
-    Hoja() {}
-
-    Hoja(int _filas, int _columnas) {
-        filas = _filas;
-        columnas = _columnas;
-        
-        matrizFinal = new int*[filas];
-        for (int i = 0; i < filas; ++i) {
-            matrizFinal[i] = new int[columnas];
-        }
-
-        matrizPrimitiva = new string*[filas];
-        for (int j = 0; j < filas; ++j) {
-            matrizPrimitiva[j] = new string[columnas];
-        }
-    }
+    Hoja(int _filas, int _columnas) : matriz(_filas, vector <string> (_columnas) ), filas(_filas), columnas(_columnas) {}
 
     void insertarDatos() {
         for (int i = 0; i < filas; ++i) {
             for (int j = 0; j < columnas; ++j) {
-                string datos;
-                cin >> datos;
-                matrizPrimitiva[i][j] = datos;
+                cin >> matriz[i][j];
             }
         }
     }
 
-    void procesarFormulas() {
+    void procesarCeldas() {
+        vector < vector <int> > temporal(filas, vector <int> (columnas, -1) );
         for (int i = 0; i < filas; ++i) {
             for (int j = 0; j < columnas; ++j) {
-                int valorCasilla;
-                if (matrizPrimitiva[i][j][0] == '=') {
-                    valorCasilla = evaluarFormula(matrizPrimitiva[i][j]);
-                } else {
-                    valorCasilla = stoi(matrizPrimitiva[i][j]);
+                if (j > 0) {
+                    cout << ' ';
                 }
-                matrizFinal[i][j] = valorCasilla;
-            }
-        }
-    }
-
-    int evaluarFormula(string formula) {
-        return 123;
-    }
-
-    void imprimirHoja() {
-        for (int i = 0; i < filas; ++i) {
-            for (int j = 0; j < columnas; ++j) {
-                cout << matrizFinal[i][j] << ' ';
+                if (matriz[i][j][0] != '=') {
+                    temporal[i][j] = atoi(matriz[i][j].c_str());
+                } else {
+                    temporal[i][j] = evaluarFormula(temporal, i, j);
+                }
+                cout << temporal[i][j];
             }
             cout << endl;
         }
     }
 
-    ~Hoja() {
-        for (int i = 0; i < filas; ++i) {
-            delete [] matrizFinal[i];
+    int evaluarFormula(vector < vector <int> > &temporal, int fil, int col) {
+        if (temporal[fil][col] != -1) {
+            return temporal[fil][col];
         }
-        delete [] matrizFinal;
+        if (matriz[fil][col][0] != '=') {
+            return temporal[fil][col] = atoi(matriz[fil][col].c_str());
+        }
+        vector < pair <int, int> > toParse;
+        concatenarFormula(matriz[fil][col], &toParse);
+        int resultado = 0;
+        for (int i = 0; i < ((int)toParse.size()); ++i) {
+            resultado += evaluarFormula(temporal, toParse[i].first, toParse[i].second);
+        }
+        return temporal[fil][col] = resultado;
+    }
 
-        for (int j = 0; j < filas; ++j) {
-            delete [] matrizPrimitiva[j];
+    void concatenarFormula(const string &formula, vector < pair<int, int> > *toParse) {
+        int i = 1;
+        while (i < ((int)formula.size())) {
+            string col, fil;
+            while (i < ((int)formula.size()) && formula[i] != '+') {
+                if (isalpha(formula[i])) {
+                    col += formula[i];
+                } else {
+                    fil += formula[i];
+                }
+                ++i;
+            }
+            toParse->push_back(make_pair(atoi(fil.c_str()) - 1, posicionColumna(col) - 1));
+            ++i;
         }
-        delete [] matrizPrimitiva;
+    }
+
+    int posicionColumna(const string &col) {
+        int posicion = 0;
+        int digitos = 1;
+        for (int i = ((int)col.size()) - 1; i >= 0; --i) {
+            posicion += (toupper(col[i]) - 'A' + 1) * digitos;
+            digitos *= 26;
+        }
+        return posicion;
     }
 
 };
@@ -86,8 +90,7 @@ int main() {
         cin >> columnas >> filas;
         Hoja hoja(filas, columnas);
         hoja.insertarDatos();
-        hoja.procesarFormulas();
-        hoja.imprimirHoja();
+        hoja.procesarCeldas();
     }
 
     return 0;
